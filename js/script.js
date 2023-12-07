b7b7f21a7ede68f97157b691f5b34bb07da3dfb4
marcelloarief <46709890+marcelloarief@users.noreply.github.com>
2023-12-07 22:02:49 +0700

    major change
    add css styling, script, 2 html page including keliling and luas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
-const inputField = document.getElementById("input-tinggi")
+function calculateLuas() {
+    var base = parseFloat(document.getElementById('base').value);
+    var height = parseFloat(document.getElementById('height').value);
 
-inputField.addEventListener('input', function(){
-    var textInput = inputField.value.trim()
-    console.log(textInput)
-})
+    if (isNaN(base) || isNaN(height)) {
+        alert("Masukkan angka yang valid untuk alas dan tinggi.");
+        return;
+    }
+
+    var area = 0.5 * base * height;
+
+    document.getElementById('areaResult').innerText = "Luas: " + area.toFixed(2);
+}
+
+function calculateKeliling() {
+    var sideA = parseFloat(document.getElementById('sideA').value);
+    var sideB = parseFloat(document.getElementById('sideB').value);
+    var sideC = parseFloat(document.getElementById('sideC').value);
+
+    if (isNaN(sideA) || isNaN(sideB) || isNaN(sideC)) {
+        alert("Masukkan angka yang valid untuk panjang sisi.");
+        return;
+    }
+
+    var perimeter = sideA + sideB + sideC;
+
+    document.getElementById('perimeterResult').innerText = "Keliling: " + perimeter.toFixed(2);
+}
